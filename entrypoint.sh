2633ac2a3dc747d2aee6fafca2baa56702ea5422
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2020-05-11 22:35:26 +0800

    add error2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -l
 echo "Hello $1"
 echo "Second arg $2"
-if ["$2" = "test"]
-then
+if ["$2" = 'test']; then
     echo "game over"
     exit 1
-fi
+else
+    echo "good"
+fi;
 time=$(date)
 echo "::set-output name=time::$time"
 printenv
