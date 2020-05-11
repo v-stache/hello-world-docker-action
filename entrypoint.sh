e0299fe19a32ab7136bed7c170009e5b3b4275bf
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2020-05-11 23:50:46 +0800

    comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 echo "Hello $1"
 echo "Second arg $2"
-if [ "$2" = "test" ]
+if [ "$2" = "test" ] # https://github.com/v-stache/hello-world-docker-action/runs/663883012?check_suite_focus=true
 then
   echo "game over"
   exit 1
